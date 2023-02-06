ae906d73192649d83be091d71eaad45724a95108
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-06 16:48:53 +0530

    Observable intro

---
+++ 05-rxjs/01-intro.js
@@ -0,0 +1,22 @@
+var { Observable } = require('rxjs')
+
+let obs$ = new Observable(subscriber => {
+    console.log('sending values')
+    subscriber.next(10)
+    subscriber.next(20)
+    subscriber.next(30)
+    subscriber.next(40)
+    // subscriber.error(new Error('invalid arguments'))
+    subscriber.complete()
+})
+
+let subscription = obs$.subscribe({
+    next : val => console.log(val),
+    complete : () => console.log('All the values received'),
+    error : err => console.log('Error :', err)
+})
+
+subscription.unsubscribe()
+
+
+Write a function that creates an observable for the given array of values
