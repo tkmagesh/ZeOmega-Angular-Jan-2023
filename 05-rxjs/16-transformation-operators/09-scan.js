c7c52c389b74bb946bcd31b27de419e8f281b78b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-13 12:12:00 +0530

    updates

--- 05-rxjs/16-transformation-operators/09-scan.js
+++ 05-rxjs/16-transformation-operators/09-scan.js
@@ -1,13 +1,17 @@
 const { from } = require('rxjs');
-const { scan } = require('rxjs/operators');
+const { scan, map } = require('rxjs/operators');
 
 // Static data source
 const source = from([1, 2, 3, 4, 5]);
 
 // Compute sum with scan operator
 const sumSubscription = source
-    .pipe(scan((acc, val) => acc + val))
-    .subscribe(val => console.log('Sum:', val));
+    .pipe(scan(({count, sum, avg}, val) => ({ 
+            count: count+1, sum : sum+val, avg : ((sum+val)/(count+1))
+        }) , { count : 0, sum : 0, avg : 0 }),
+        map(({avg}) => avg)
+    )
+    .subscribe(val => console.log('Avg:', val));
 
 // The simulation
 setTimeout(() => sumSubscription.unsubscribe());
