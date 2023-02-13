1618897f16f63d064210b513e80f56a3b25086c4
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-13 12:39:03 +0530

    combineLatestWith

---
+++ 05-rxjs/17-combinational-operators/01-combineLatestWith.js
@@ -0,0 +1,33 @@
+const { combineLatestWith, Subject } = require('rxjs');
+
+const subject1 = new Subject();
+const subject2 = new Subject();
+const subject3 = new Subject();
+
+/* 
+combineLatestWith(subject1, subject2, subject3).subscribe(val => {
+    console.log(val);
+}) 
+*/
+subject1
+    .pipe(
+        combineLatestWith(subject2, subject3)
+    )
+    .subscribe(console.log)
+
+setTimeout(() => {
+    subject1.next(1);
+}, 500)
+
+setTimeout(() => {
+    subject2.next(2);
+}, 500)
+
+setTimeout(() => {
+    subject3.next(3);
+}, 700)
+
+
+setTimeout(() => {
+    subject3.next('3 again');
+}, 1200)
