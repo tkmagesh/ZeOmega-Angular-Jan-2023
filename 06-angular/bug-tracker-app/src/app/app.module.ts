5b342bfd9814ef8c126bb04dc3d3b7e5ac1e59d4
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-15 14:55:12 +0530

    Improve the display of createdAt for each bug (using custom pipe)

--- 06-angular/bug-tracker-app/src/app/app.module.ts
+++ 06-angular/bug-tracker-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BugsComponent } from './bugs/bugs.component';
+import { ElapsedPipe } from './bugs/pipes/elapsed.pipe';
 import { BugOperationsService } from './bugs/services/bugOperations.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ElapsedPipe,
     BugsComponent
   ],
   imports: [
