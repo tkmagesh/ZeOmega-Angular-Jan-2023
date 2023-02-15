207f079eac64fcdca5d8846a2da35eff414cb8cf
Magesh Kuppan <tkmagesh77@gmail.com>
2023-02-15 11:47:45 +0530

    Introducing the Model (Bug)

--- 06-angular/bug-tracker-app/src/app/bugs/bugs.component.ts
+++ 06-angular/bug-tracker-app/src/app/bugs/bugs.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { Bug } from "./models/bug";
+
+
 
 @Component({
     selector : 'app-bugs',
@@ -6,16 +9,25 @@ import { Component } from "@angular/core";
     styleUrls : ['bugs.component.css']
 })
 export class BugsComponent{
-    bugs : string[] = [
+    bugs : Bug[] = [
         
     ]
 
+    private _currentBugId : number = 0
+
     onBtnAddNewClick(newBugName : string){
-        this.bugs.push(newBugName)
+        const newBug : Bug = {
+            id : ++this._currentBugId,
+            name : newBugName,
+            isClosed : false,
+            createdAt : new Date()
+        } 
+        this.bugs.push(newBug)
+
     }
 
-    onBtnRemoveClick(bugToRemove : string){
-        this.bugs = this.bugs.filter(bugName => bugName != bugToRemove)
+    onBtnRemoveClick(bugToRemove : Bug){
+        // this.bugs = this.bugs.filter(bugName => bugName != bugToRemove)
     }
 
 }
